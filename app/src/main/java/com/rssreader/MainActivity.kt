3c0f708434b23a4bdc5b8f7b093fbfc5bf8c3ddf
svvashishtha <saurabh.vashisht@zomato.com>
2020-09-10 19:27:04 +0530

    Added basic structure for Day night theme. Added navigation on FAB click to AddNewFeedFragment.kt Hide fab when AddNewFeedFragment.kt is opened. Show fab again when Back is pressed.

--- app/src/main/java/com/rssreader/MainActivity.kt
+++ app/src/main/java/com/rssreader/MainActivity.kt
@@ -2,10 +2,88 @@ package com.rssreader
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import com.google.android.material.transition.MaterialElevationScale
+import com.rssreader.databinding.ActivityMainBinding
+import com.rssreader.util.contentView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+    private val binding: ActivityMainBinding by contentView(R.layout.activity_main)
+
+    val currentNavigationFragment: Fragment?
+        get() = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+            ?.childFragmentManager
+            ?.fragments
+            ?.first()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setUpFab()
+    }
+
+    private fun setUpFab() {
+        // Wrap binding.run to ensure ContentViewBindingDelegate is calling this Activity's
+        // setContentView before accessing views
+        binding.run {
+            findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener(
+                this@MainActivity
+            )
+        }
+
+        // Set a custom animation for showing and hiding the FAB
+        binding.fab.apply {
+            setShowMotionSpecResource(R.animator.fab_show)
+            setHideMotionSpecResource(R.animator.fab_hide)
+            setOnClickListener {
+                navigateToCompose()
+            }
+        }
+    }
+
+    private fun navigateToCompose() {
+        currentNavigationFragment?.apply {
+            exitTransition = MaterialElevationScale(false).apply {
+                duration = resources.getInteger(R.integer.rss_reader_motion_duration_large).toLong()
+            }
+            reenterTransition = MaterialElevationScale(true).apply {
+                duration = resources.getInteger(R.integer.rss_reader_motion_duration_large).toLong()
+            }
+        }
+        val directions = NavigationGraphDirections.actionAddFeed()
+        findNavController(R.id.nav_host_fragment).navigate(directions)
+    }
+
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        when (destination.id) {
+            R.id.addNewFeedFragment -> {
+                setUpActivityForAddNewFeedFragment()
+            }
+            R.id.myFeedsFragment -> {
+                setUpActivityForMyFeeds()
+            }
+            R.id.feedItemDescriptionFragment -> {
+            }
+            R.id.rssFeedFragment -> {
+            }
+        }
+    }
+
+    private fun setUpActivityForMyFeeds() {
+        binding.run{
+        binding.fab.show()}
+    }
+
+    private fun setUpActivityForAddNewFeedFragment() {
+        binding.run{
+            binding.fab.hide()
+        }
     }
 }
--- app/src/main/java/com/rssreader/ui/AddNewFeedFragment.kt
+++ app/src/main/java/com/rssreader/ui/AddNewFeedFragment.kt
@@ -1,60 +1,53 @@
 package com.rssreader.ui
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.transition.Slide
+import com.google.android.material.transition.MaterialContainerTransform
 import com.rssreader.R
+import com.rssreader.databinding.FragmentAddNewFeedBinding
+import kotlinx.android.synthetic.main.fragment_add_new_feed.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddNewFeedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddNewFeedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var binding: FragmentAddNewFeedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_new_feed, container, false)
+        binding = FragmentAddNewFeedBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddNewFeedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddNewFeedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        enterTransition = MaterialContainerTransform().apply {
+            startView = requireActivity().findViewById(R.id.fab)
+            endView = add_new_feed_view
+            duration = resources.getInteger(R.integer.rss_reader_motion_duration_large).toLong()
+            scrimColor = Color.TRANSPARENT
+//            containerColor = requireContext().themeColor(R.attr.colorSurface)
+//            startContainerColor = requireContext().themeColor(R.attr.colorSecondary)
+//            endContainerColor = requireContext().themeColor(R.attr.colorSurface)
+        }
+        returnTransition = Slide().apply {
+            duration = resources.getInteger(R.integer.rss_reader_motion_duration_medium).toLong()
+            addTarget(R.id.add_new_feed_view)
+        }
+        exitTransition = MaterialContainerTransform().apply {
+            endView = requireActivity().findViewById(R.id.fab)
+            startView = add_new_feed_view
+            duration = resources.getInteger(R.integer.rss_reader_motion_duration_large).toLong()
+            scrimColor = Color.TRANSPARENT
+        }
     }
 }
