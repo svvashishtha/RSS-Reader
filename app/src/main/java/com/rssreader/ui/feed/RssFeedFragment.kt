91b587159acbdc4409b2a577d13d95d78ecd975b
svvashishtha <saurabh.vashisht@zomato.com>
2020-10-02 20:29:58 +0530

    Added shared element transition to RssFeedFragment.kt

--- app/src/main/java/com/rssreader/ui/feed/RssFeedFragment.kt
+++ app/src/main/java/com/rssreader/ui/feed/RssFeedFragment.kt
@@ -1,11 +1,17 @@
 package com.rssreader.ui.feed
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.transition.MaterialContainerTransform
+import com.rssreader.R
 import com.rssreader.databinding.FragmentRssFeedBinding
+import com.rssreader.util.themeColor
 
 /**
  * Shows the RSS feed of a channel selected.
@@ -13,13 +19,29 @@ import com.rssreader.databinding.FragmentRssFeedBinding
 class RssFeedFragment : Fragment() {
 
     private lateinit var binding: FragmentRssFeedBinding
+    private val args: RssFeedFragmentArgs by navArgs()
+    private val channel by lazy { args.channel }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = MaterialContainerTransform().apply {
+            drawingViewId = R.id.nav_host_fragment
+            duration = resources.getInteger(R.integer.rss_reader_motion_duration_large).toLong()
+            scrimColor = Color.TRANSPARENT
+            setAllContainerColors(requireContext().themeColor(R.attr.colorSurface))
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRssFeedBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        channel.title?.let { Log.d("RssFeedFragment", it) }
     }
 
 }
